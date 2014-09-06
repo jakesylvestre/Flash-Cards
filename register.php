fd547763c0ca68bf105d9cad3edf30264b538285
Jake Sylvestre <jakesyl@gmail.com>
2014-09-05 23:30:38 -0400

    Added registration page

---
+++ register.php
@@ -0,0 +1,20 @@
+<!-- 
+Design register pure CSS
+Developed by @mrjopino
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Register</title>
+<link rel="stylesheet" type="text/css" href="css/register.css">
+</head>
+<p class="texto">Register</p>
+<div class="Register">
+<form method="post" action="javascript:void(0);">
+<span class="fontawesome-user"></span><input type="text" required placeholder="Username" autocomplete="off"> 
+<span class="fontawesome-envelope-alt"></span><input type="text" id="email" required placeholder="Email" autocomplete="off">
+<span class="fontawesome-lock"></span><input type="password" name="password" id="password" required placeholder="Contraseña" autocomplete="off"> 
+			<input type="submit" value="Registrar" title="Registra tu cuenta">
+</form>
+</div>
+</html>
