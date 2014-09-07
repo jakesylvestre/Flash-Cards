1fb08c4cf751403f5b24380071bd57116b2ca1c1
Jake Sylvestre <jakesyl@gmail.com>
2014-09-07 00:37:21 -0400

    Finished regastration page

--- register.php
+++ register.php
@@ -10,11 +10,11 @@ Developed by @mrjopino
 </head>
 <p class="texto">Register</p>
 <div class="Register">
-<form method="post" action="register_login.php">
-<span class="fontawesome-user"></span><input type="text" required placeholder="Username" autocomplete="off"> 
-<span class="fontawesome-envelope-alt"></span><input type="text" id="email" required placeholder="Email" autocomplete="off">
-<span class="fontawesome-lock"></span><input type="password" name="password" id="password" required placeholder="Contraseña" autocomplete="off"> 
-<input type="submit" value="Registrar" title="Registra tu cuenta">
+<form method="post" action="register_check.php">
+<span class="fontawesome-user"></span><input type="text" id = "myusername" name="username" required placeholder="Username" autocomplete="off"> 
+<span class="fontawesome-envelope-alt"></span><input type="text" id="myemail" required placeholder="Email" autocomplete="off">
+<span class="fontawesome-lock"></span><input type="password" name="password" id="mypassword" required placeholder="Contraseña" autocomplete="off"> 
+<input type="submit" value="Registrar" title="Register">
 </form>
 </div>
 </html> 
--- register_check.php
+++ register_check.php
@@ -4,20 +4,21 @@ id int,
 username varchar(255),
 password varchar(255)
 */
-require 'test_password.php';
+//TODO add emails
 
+require 'test_password.php';
 ob_start();//Don't output anything
 
 //Adding sql credentials
 $host="localhost";
 $username="replace_this";
-$password="replace_me"; 
-$db_name="replace_this"; 
+$password="replace_me";
+$db_name="replace_this";
 $tbl_name="members";
 
 // Connect to server and select databse.
 $link = mysqli_connect($host, $username, $password, $db_name);//Add or die please
-// Check connection 
+// Check connection
 
 
 if (mysqli_connect_errno()) {//Error TODO try to reconnect and put this in a loop
@@ -25,38 +26,28 @@ if (mysqli_connect_errno()) {//Error TODO try to reconnect and put this in a loo
     exit();
 }
 
-$myusername=$_POST['myusername']; 
-$mypassword=$_POST['mypassword']; 
+$myemail=$_POST['myemail'];
+$myusername=$_POST['myusername'];
+$mypassword=$_POST['mypassword'];
 
 // To protect SQL injection (more detail about MySQL injection)
 $myusername = stripslashes($myusername);
 $mypassword = stripslashes($mypassword);
+$myemail = stripslashes($myemail);
 
 $myusername = mysqli_real_escape_string($link, $myusername);
 $mypassword = mysqli_real_escape_string($link, $mypassword);
+$myemail = mysqli_real_escape_string($link, $myemail);
 
 $mypassword = password_hash($mypassword, PASSWORD_DEFAULT);
 
-$sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
+$sql="INSERT INTO $tbl_name VALUES(username='$myusername' and password='$mypassword')";
 
-$result=mysqli_query($link, $sql);
+mysqli_query($link, $sql);
 
 // Mysql_num_row is counting table row
-$count=mysqli_num_rows($result);
 
 // If result matched $myusername and $mypassword, table row must be 1 row
-if($count==1){
-
-// Register $myusername, $mypassword and redirect to file "login_success.php"
- 
-session_register("myusername");
-session_register("mypassword"); 
-
-header("location:login_success.php");
-}
-else {
-echo "Wrong Username or Password";
-}
 mysqli_close($link); //"Fuck that connection"
 
 ob_end_flush();
